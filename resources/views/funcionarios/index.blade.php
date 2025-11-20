bd35e7bb76c4618d76dfbdf7486e917b4914a9a2
lorenaemily <lorena.lima47@aluno.ce.gov.br>
2025-11-20 15:33:05 -0300

    alterada a view index de funcionários, com adição dos campos cpf,email,endereço e serviço

--- resources/views/funcionarios/index.blade.php
+++ resources/views/funcionarios/index.blade.php
@@ -3,21 +3,34 @@
 @section('content')
 <h1>Lista de Funcionários</h1>
 
-<a href="{{route('funcionarios.create')}}">Novo Funcionário</a>
+<a href="{{ route('funcionarios.create') }}">Novo Funcionário</a>
+
 @if (session('message'))
     <div>{{ session('message') }}</div>
 @endif
 
 <ul>
-
 @foreach ($funcionarios as $funcionario)
     <li>
-        {{ $funcionario->nome }} - {{ $funcionario->cargo }} - {{ $funcionario->salario }} - {{ $funcionario->telefone }}
+        {{ $funcionario->nome }} -
+        {{ $funcionario->servico ? $funcionario->servico->nome : 'Sem serviço' }} -
+        {{ $funcionario->salario }} -
+        {{ $funcionario->telefone }} -
+        {{ $funcionario->email }} -
+        {{ $funcionario->endereco }} -
+        {{ $funcionario->cpf }}
+        |
+
         <a href="{{ route('funcionarios.edit', $funcionario->id) }}">Editar</a>
-        <form action="{{ route('funcionarios.destroy', $funcionario->id) }}" method="POST" style="display:inline;">
+
+        <form action="{{ route('funcionarios.destroy', $funcionario->id) }}"
+              method="POST" style="display:inline;">
             @csrf
             @method('DELETE')
-            <button type="submit" onclick="return confirm('Tem certeza que deseja excluir este funcionário ?')">Excluir</button>
+            <button type="submit"
+                onclick="return confirm('Tem certeza que deseja excluir este funcionário ?')">
+                Excluir
+            </button>
         </form>
     </li>
 @endforeach
