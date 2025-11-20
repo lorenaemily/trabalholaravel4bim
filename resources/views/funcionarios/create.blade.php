c5f34634537f8acd384647540a73e23fb3d9cd5e
lorenaemily <lorena.lima47@aluno.ce.gov.br>
2025-11-20 15:40:51 -0300

    view create de funcionários atualizada com os novos campos incluindo o serviço

--- resources/views/funcionarios/create.blade.php
+++ resources/views/funcionarios/create.blade.php
@@ -13,14 +13,26 @@
         <label>Nome:</label>
         <input type="text" name="nome" required><br><br>
 
-        <label>Cargo:</label>
-        <input type="text" name="cargo" required><br><br>
+        <label>CPF:</label>
+        <input type="text" name="cpf" required><br><br>
+
+        <label>Email:</label>
+        <input type="email" name="email" required><br><br>
 
         <label>Telefone:</label>
         <input type="text" name="telefone"><br><br>
 
         <label>Salário:</label>
-        <input type="int" name="salario"><br><br>
+        <input type="number" step="0.01" name="salario" required><br><br>
+
+        <label>Serviço:</label>
+        <select name="servico_id" required>
+            <option value="">Selecione um serviço</option>
+            @foreach($servicos as $servico)
+                <option value="{{ $servico->id }}">{{ $servico->nome }}</option>
+            @endforeach
+        </select>
+        <br><br>
 
         <button type="submit">Salvar</button>
     </form>
