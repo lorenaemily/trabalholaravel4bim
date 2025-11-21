a93a13520a4f36f79bbe5b9305a06ede517f49d4
lorenaemily <lorena.lima47@aluno.ce.gov.br>
2025-11-21 11:55:39 -0300

    view edit de funcionários atualizada para comportar os novos campos

--- resources/views/funcionarios/edit.blade.php
+++ resources/views/funcionarios/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.app')
 
 @section('content')
-    <h1>Editar Dados do Funcionário</h1>
+    <h1>Editar Funcionário</h1>
 
     {{-- Mensagem de sucesso --}}
     @if (session('message'))
@@ -24,19 +24,57 @@
         @csrf
         @method('PUT')
 
-        <label for="nome">Nome:</label><br>
-        <input type="text" name="nome" id="nome" value="{{ old('nome', $funcionario->nome) }}" required><br><br>
+        <label>Nome:</label>
+        <input type="text"
+               name="nome"
+               value="{{ old('nome', $funcionario->nome) }}"
+               required>
+        <br><br>
 
-        <label for="cargo">cargo:</label><br>
-        <input type="text" name="cargo" id="cargo" value="{{ old('cargo', $funcionario->cargo) }}" required><br><br>
+        <label>CPF:</label>
+        <input type="text"
+               name="cpf"
+               value="{{ old('cpf', $funcionario->cpf) }}"
+               required>
+        <br><br>
 
-        <label for="telefone">Telefone:</label><br>
-        <input type="text" name="telefone" id="telefone" value="{{ old('telefone', $funcionario->telefone) }}"><br><br>
+        <label>Email:</label>
+        <input type="email"
+               name="email"
+               value="{{ old('email', $funcionario->email) }}"
+               required>
+        <br><br>
 
-        <label for="salario">Salário:</label><br>
-        <input type="int" name="salario" id="salario" value="{{ old('salario', $funcionario->salario) }}"><br><br>
+        <label>Telefone:</label>
+        <input type="text"
+               name="telefone"
+               value="{{ old('telefone', $funcionario->telefone) }}">
+        <br><br>
 
-        <button type="submit">Salvar Alterações</button>
-        <a href="{{ route('funcionarios.index') }}">Voltar</a>
+        <label>Salário:</label>
+        <input type="number"
+               step="0.01"
+               name="salario"
+               value="{{ old('salario', $funcionario->salario) }}"
+               required>
+        <br><br>
+
+        <label>Serviço:</label>
+        <select name="servico_id" required>
+            <option value="">Selecione um serviço</option>
+
+            @foreach($servicos as $servico)
+                <option value="{{ $servico->id }}"
+                    {{ old('servico_id', $funcionario->servico_id) == $servico->id ? 'selected' : '' }}>
+                    {{ $servico->nome }}
+                </option>
+            @endforeach
+        </select>
+        <br><br>
+
+        <button type="submit">Salvar alterações</button>
     </form>
+
+    <br>
+    <a href="{{ route('funcionarios.index') }}">Voltar</a>
 @endsection
