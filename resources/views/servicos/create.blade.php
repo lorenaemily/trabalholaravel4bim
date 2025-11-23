df3ecf14b329f7bc0ddf7b3f94378095fcb4ce84
lorenaemily <lorena.lima47@aluno.ce.gov.br>
2025-11-23 11:11:15 -0300

    view create de serviços implementada

---
+++ resources/views/servicos/create.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastrar Serviço</title>
+</head>
+<body>
+    <h1>Cadastro de Serviço</h1>
+
+    <form action="{{ route( Servicos.store') }}" method="POST">
+        @csrf
+
+        <label>Nome:</label>
+        <input type="text" name="nome" required><br><br>
+
+        <label>Preço:</label>
+        <input type="number" step="0.01" name="preco" required><br><br>
+
+        <label>Descrição:</label>
+        <input type="text" name="descricao"><br><br>
+
+        <button type="submit">Salvar</button>
+    </form>
+
+    <a href="{{ route( Servicos.index') }}">Voltar</a>
+</body>
+</html>
